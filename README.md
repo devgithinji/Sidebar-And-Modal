2ef56b8e844cc202887705a5276d416e70c6f8c7
dennis <wakahiad@gmail.com>
2022-06-02 13:17:37 +0300

    final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sidebar and Modal Demo
+
+Application developed in React js
+
+Click [here](https://sidebar-modal-demo.netlify.app) to view application
+
+Dennis Githinji 👀️
--- src/context.js
+++ src/context.js
@@ -1,1 +1,42 @@
-import React, { useState, useContext } from 'react'
+import React, {useState, useContext, createContext} from 'react'
+
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+    const [isSideBarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+
+
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+
+
+    return <AppContext.Provider value={{
+        isModalOpen,
+        isSideBarOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar
+    }}>{children}</AppContext.Provider>
+}
+
+//custom hook
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export {AppContext, AppProvider}
